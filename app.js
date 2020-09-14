cb933a03d4dd26064def37d70c44f34264db0166
Ohzzi <clover7even@naver.com>
2020-09-14 14:19:08 +0900

    make server

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const express = require('express')
+const socket = require('socket.io')
+const https = require('https')
+const fs = require('fs')
+
+const option = {
+    key: fs.readFileSync('C:/Users/오지훈/Desktop/Jihoon/ssl/localhost_private.key'),
+    cert: fs.readFileSync('C:/Users/오지훈/Desktop/Jihoon/ssl/localhost.crt')
+}
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hi')
+})
+
+https.createServer(option, app).listen(443, (req, res) => {
+    console.log('server start')
+})
