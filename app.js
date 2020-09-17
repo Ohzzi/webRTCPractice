223a9828666d188be26c42e2eae2d211c838a686
Oh <clover7even@naver.com>
2020-09-17 15:57:17 +0900

    modify .gitignore, ejs

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const router = require(__dirname + '/router/main.js')
 const session = require('express-session')
 
 const option = {
-    key: fs.readFileSync('C:/Users/오지훈/Desktop/Jihoon/ssl/localhost_private.key'),
-    cert: fs.readFileSync('C:/Users/오지훈/Desktop/Jihoon/ssl/localhost.crt')
+    key: fs.readFileSync('ssl/localhost_private.key'),
+    cert: fs.readFileSync('ssl/localhost.crt')
 }
 
 const bodyParser = require('body-parser')
@@ -29,6 +29,7 @@ app.use('/js', express.static('./static/js'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use(session({
     secret: '@#@$MYSIGN#@$#$',
     resave: false,
@@ -39,8 +40,46 @@ app.use('/', router)
 
 io.sockets.on('connection', (socket) => {
     console.log('유저 접속 됨')
+
+    function log() {
+        let array = ['Message from Server: ']
+        array.push.apply(array,arguments)
+        socket.emit('log',array)
+    }
+
+    socket.on('send', (data) => {
+        console.log(data.message)
+    })
     
     socket.on('disconnect', () => {
         console.log('접속 종료')
     })
+
+    socket.on('message', (message) =>{
+        log('Client said : ' , message)
+        socket.broadcast.emit('message', message)
+    });
+
+    /*socket.on('create or join',room=>{
+        let clientsInRoom = io.sockets.adapter.rooms[room]
+        let numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0
+        log('Room ' + room + ' now has ' + numClients + ' client(s)')
+        
+        if(numClients === 0){
+            console.log('create room!')
+            socket.join(room);
+            log('Client ID ' + socket.id + ' created room ' + room)
+            socket.emit('created',room,socket.id)
+        }
+        else if(numClients===1){
+            console.log('join room!')
+            log('Client Id' + socket.id + 'joined room' + room)
+            io.sockets.in(room).emit('join',room)
+            socket.join(room)
+            socket.emit('joined',room,socket.id)
+            io.sockets.in(room).emit('ready')
+        }else {
+            socket.emit('full',room);
+        }
+    })*/
 })
--- static/js/rtc.js
+++ static/js/rtc.js
@@ -1,6 +1,5 @@
 let localVideo = document.getElementById('localVideo'),
     remoteVideo = document.getElementById('remoteVideo'),
-    localstream,
     isInitiator = false,
     isChannelReady = false,
     isStarted = false,
@@ -8,10 +7,10 @@ let localVideo = document.getElementById('localVideo'),
     remoteStream,
     pc
 
-let socket = io()
+const socket = io()
 
 socket.on('connect', () => {
-    socket.emit('send', 'connected')
+    socket.emit('send', {message: 'connected'})
 })
 
 navigator.mediaDevices.getUserMedia({
@@ -26,8 +25,8 @@ navigator.mediaDevices.getUserMedia({
 
 function gotStream(stream) {
     console.log("Adding local stream")
-    localStream = stream;
-    localVideo.srcObject = stream;
+    localStream = stream
+    localVideo.srcObject = stream
     sendMessage("got user media")
     if (isInitiator) {
         maybeStart()
@@ -35,6 +34,108 @@ function gotStream(stream) {
 }
 
 function sendMessage(message) {
-    console.log('Client sending message: ', message)
-
+    console.log("Client sending message: ", message)
+    socket.emit('message', message)
 }
+
+/*function maybeStart() {
+    console.log(">>>>>> maybeStart()", isStarted, localStream, isChannelReady)
+    if (!isStarted && typeof (localStream) !== 'undefined' && isChannelReady) {
+        console.log(">>>>>> Creating peer connection")
+        createPeerConnection()
+        pc.addStream(localStream)
+        isStarted = true
+        console.log('initiator', isInitiator)
+        if (isInitiator) {
+            doCall()
+        }
+    }
+}
+
+function createPeerConnection() {
+    try {
+        pc = new RTCPeerConnection(pcConfig)
+        pc.onicecandidate = handleIceCandidate
+        pc.onaddstream = handleRemoteStreamAdded
+        pc.onremovestream = handleRemoteStreamRemoved
+    } catch (error) {
+        console.log("Failed to create PeerConnection, exception: ", error.message)
+        alert("Cannot create PeerConnection object")
+        return
+    }
+}
+
+function handleIceCandidate(event) {
+    console.log("iceCandidateEvent", event)
+    if (event.candidate) {
+        sendMessage({
+            type: 'candidate',
+            label: event.candidate.sdpMLineIndex,
+            id: event.candidate.sdpMid,
+            candidate: event.candidate.candidate,
+        })
+    } else {
+        console.log("end of candidates")
+    }
+}
+
+function handleRemoteStreamAdded(event) {
+    console.log("createOffer() error: ", event)
+}
+
+function handleRemoteStreamAdded(event) {
+    console.log("remote stream added")
+    remoteStream = event.stream
+    remoteVideo.srcObject = remoteStream
+}
+
+function doCall() {
+    console.log("Sending offer to peer")
+    pc.createOffer(setLocalAndSendMessage, handleCreateOfferError)
+}
+
+function doAnswer() {
+    console.log("Sending answer to peer")
+    pc.createAnswer().then(
+        setLocalAndSendMessage,
+        onCreateSessionDescriptionError
+    )
+}
+
+function setLocalAndSendMessage(sessionDescription) {
+    pc.setLocalDescription(sessionDescription)
+    sendMessage(sessionDescription)
+}
+
+function setLocalAndSendMessage(sessionDescription) {
+    pc.setLocalDescription(sessionDescription)
+    sendMessage(sessionDescription)
+}
+
+let pcConfig = {
+    'iceServers': [{
+        'urls': 'stun:stun.l.google.com:19302'
+      }]
+}
+
+socket.on('message', (message)=>{
+  console.log('Client received message :',message)
+  if(message === 'got user media'){
+    maybeStart()
+  }else if(message.type === 'offer'){
+    if(!isInitiator && !isStarted){
+      maybeStart()
+    }
+    pc.setRemoteDescription(new RTCSessionDescription(message))
+    doAnswer()
+  }else if(message.type ==='answer' && isStarted){
+    pc.setRemoteDescription(new RTCSessionDescription(message))
+  }else if(message.type ==='candidate' &&isStarted){
+    const candidate = new RTCIceCandidate({
+      sdpMLineIndex : message.label,
+      candidate:message.candidate
+    })
+
+    pc.addIceCandidate(candidate)
+  }
+})*/
