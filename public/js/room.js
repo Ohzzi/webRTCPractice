b113e890da32dea0eda1482cbb52ecea6a235ba3
Oh <clover7even@naver.com>
2020-09-21 13:15:06 +0900

    refactor: Use async await

--- public/js/room.js
+++ public/js/room.js
@@ -5,6 +5,7 @@ const constraints = {
 }
 const myVideo = document.createElement('video')
 
+/* promise
 navigator.mediaDevices.getUserMedia(constraints)
     .then((stream) => {
         console.log('Got MediaStream', stream)
@@ -13,6 +14,20 @@ navigator.mediaDevices.getUserMedia(constraints)
     .catch((error) => {
         console.log('Error: ', error)
     })
+*/
+
+playLocalVideo()
+
+/* async */
+async function playLocalVideo() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints)
+        console.log('Got MediaStream', stream)
+        addVideoStream(myVideo, stream)
+    } catch (error) {
+        console.log('Error: ', error)        
+    }
+}
 
 function addVideoStream(video, stream) {
     video.srcObject = stream
