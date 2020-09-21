a30d9b865d92c47e23af18e8cc853110020a24c9
Oh <clover7even@naver.com>
2020-09-21 10:33:05 +0900

    feat: Add room.ejs and css
    room.ejs will represent user's local video and remote video.

--- router/index.js
+++ router/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const signUp = require('./signup/index')
+const room = require('./room/index')
 
 router.get('/', (req, res) => {
     res.render('index')
 })
 
 router.use('/signup', signUp)
+router.use('/room', room)
 
 module.exports = router
