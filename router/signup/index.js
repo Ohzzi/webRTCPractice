61f66e188754d03063a73e5748af291dcfe44631
Oh <clover7even@naver.com>
2020-09-21 09:01:14 +0900

    feat: Connect to mariaDB

--- router/signup/index.js
+++ router/signup/index.js
@@ -1,7 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const mariaDB = require('mariadb')
 
-router.get('/', (req,res) => {
+const pool = mariaDB.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'dhwlgns97@',
+    database: 'webrtc'
+})
+
+async function selectAll() {
+    let conn;
+    try {
+  
+      conn = await pool.getConnection()
+      const rows = await conn.query("SELECT * from USER")
+      console.log(rows)
+  
+    } catch (err) {
+      throw err
+    } finally {
+      if (conn) conn.release() //release to pool
+    }
+}
+
+router.get('/', (req, res) => {
     res.render('signup')
 })
 
@@ -11,6 +35,8 @@ router.post('/', (req, res) => {
     const pw = body.password
     const name = body.name
     const email = body.email
+
+    selectAll()
 })
 
 module.exports = router
