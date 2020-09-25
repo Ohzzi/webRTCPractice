3eb17595da813aece7e3335fb50e80fa63056648
Oh <clover7even@naver.com>
2020-09-25 13:40:53 +0900

    feat: Add remote stream Can use peer-to-peer video chat

--- public/js/room.js
+++ public/js/room.js
@@ -1,6 +1,8 @@
-let isCaller;
+
+let localStream, remoteStream, pc, isCaller = false;
 
 const videoGrid = document.getElementById('video-grid');
+const remoteVideo = document.createElement('video');
 
 const startBtn = document.getElementById('startButton');
 startBtn.addEventListener('click', playLocalVideo);
@@ -10,8 +12,8 @@ callBtn.addEventListener('click', call);
 
 const iceServers = {
     'iceServers': [
-        {'urls': 'stun:stun.l.google.com:19302'},
-        {'urls': 'stun:stun.services.mozila.com'}
+        { 'urls': 'stun:stun.l.google.com:19302' },
+        { 'urls': 'stun:stun.services.mozila.com' }
     ]
 }
 
@@ -20,15 +22,7 @@ const constraints = {
     'audio': true
 }
 
-async function playLocalVideo() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        const myVideo = document.createElement('video');
-        addVideoStream(myVideo, stream);
-        isCaller = true;
-    } catch (error) {
-        console.log('error: ', error);
-    }
+function playLocalVideo() {
 }
 
 function addVideoStream(video, stream) {
@@ -46,6 +40,105 @@ const socket = io();
 socket.emit('create or join', ROOM_ID, USER_ID);
 
 socket.on('created', room => {
-    console.log('created');
-    isCaller = true;
+    const myVideo = document.createElement('video');
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(stream => {
+            localStream = stream;
+            addVideoStream(myVideo, stream);
+            isCaller = true;
+            console.log('created');
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+socket.on('joined', room => {
+    const myVideo = document.createElement('video');
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(stream => {
+            localStream = stream;
+            addVideoStream(myVideo, stream);
+            socket.emit('ready', room);
+            console.log('joined');
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+socket.on('ready', () => {
+    if (isCaller) {
+        pc = new RTCPeerConnection(iceServers);
+        pc.onicecandidate = onIceCandidate;
+        pc.ontrack = onAddStream;
+        pc.addTrack(localStream.getTracks()[0], localStream);
+        pc.addTrack(localStream.getTracks()[1], localStream);
+        pc.createOffer()
+            .then(sessionDescription => {
+                pc.setLocalDescription(sessionDescription);
+                socket.emit('offer', {
+                    type: 'offer',
+                    sdp: sessionDescription,
+                    room: ROOM_ID
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+});
+
+socket.on('offer', event => {
+    if (!isCaller) {
+        pc = new RTCPeerConnection(iceServers);
+        pc.onicecandidate = onIceCandidate;
+        pc.ontrack = onAddStream;
+        pc.addTrack(localStream.getTracks()[0], localStream);
+        pc.addTrack(localStream.getTracks()[1], localStream);
+        pc.setRemoteDescription(new RTCSessionDescription(event));
+        pc.createAnswer()
+            .then(sessionDescription => {
+                pc.setLocalDescription(sessionDescription);
+                socket.emit('answer', {
+                    type: 'answer',
+                    sdp: sessionDescription,
+                    room: ROOM_ID
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+});
+
+socket.on('answer', event => {
+    pc.setRemoteDescription(new RTCSessionDescription(event));
+});
+
+socket.on('candidate', event => {
+    const candidate = new RTCIceCandidate({
+        sdpMLineIndex: event.label,
+        candidate: event.candidate
+    });
+    pc.addIceCandidate(candidate);
 })
+
+function onAddStream(event) {
+    console.log(event.streams[0]);
+    addVideoStream(remoteVideo, event.streams[0]);
+    remoteStream = event.streams[0];
+}
+
+function onIceCandidate(event) {
+    if (event.candidate) {
+        console.log('sending ice candidate', event.candidate);
+        socket.emit('candidate', {
+            type: 'candidate',
+            label: event.candidate.sdpMLineIndex,
+            id: event.candidate.sdpMid,
+            candidate: event.candidate.candidate,
+            room: ROOM_ID
+        });
+    }
+}
